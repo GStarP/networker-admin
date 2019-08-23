8a2027e1c7d472731f04655f606ac462987027b7
GStarP <171250605@smail.nju.edu.cn>
2019-08-23 22:26:22 +0800

    feat: 添加公司信息列表和分页

--- src/views/CompanyInfo.vue
+++ src/views/CompanyInfo.vue
@@ -13,7 +13,6 @@
           solo
           rounded
           clearable
-          autofocus
         ></v-text-field>
       </v-flex>
       <v-flex xs12 sm12 md10 lg10>
@@ -26,7 +25,31 @@
             :size="60"
             indeterminate
           ></v-progress-circular>
-          <!-- TODO: 公司信息列表 -->
+          <!-- 公司信息列表 -->
+          <v-list>
+            <template
+              v-for="(item, i) of companyInfoList"
+            >
+              <v-list-item
+                class="company-info-item"
+                :key="item.id"
+              >
+
+              </v-list-item>
+              <v-divider :key="`divider-${i}`" v-if="i != (companyInfoList.length - 1)"></v-divider>
+            </template>
+          </v-list>
+          <!-- 分页 -->
+          <!-- 用一个 div 包裹防止其无视边距的限制而不适配手机端 -->
+          <div class="company-info-pagination-container">
+            <v-pagination
+              v-if="requirePagination"
+              v-model="pageNum"
+              :length="totalPages"
+              :total-visible="totalPages"
+              @input="fetchPage()"
+          ></v-pagination>
+          </div>
         </v-card>
       </v-flex>
     </v-layout>
@@ -39,17 +62,63 @@ import { mapState, mapMutations } from 'vuex';
 export default {
   data () {
     return {
-      keyword: '' // 搜索关键词
+      pageNum: 1, // 当前页码
+      pageSize: 5, // 页大小
+      totalPages: 20, // 总页数
+      keyword: '', // 搜索关键词
+      // 当前页的公司信息列表
+      companyInfoList: [
+        {
+          id: 996,
+          name: '郝呆科技'
+        },
+        {
+          id: 309,
+          name: '紫宁娱乐'
+        },
+        {
+          id: 1004,
+          name: '小草影业'
+        },
+        {
+          id: 2000,
+          name: '紫婷服装'
+        },
+        {
+          id: 777,
+          name: '小七电竞'
+        }
+      ]
     };
   },
   computed: {
-    ...mapState(['compInfoLoading'])
+    ...mapState(['compInfoLoading']),
+    // 是否需要显示分页
+    requirePagination () {
+      if (this.companyInfoList === undefined) {
+        // 公司信息列表未定义
+        return false;
+      } else if (this.companyInfoList.length < this.pageSize) {
+        // 公司信息不足一页
+        return false;
+      } else if (this.totalPages === 1) {
+        // 公司信息恰好一页
+        return false;
+      } else {
+        // 其他情况需要显示分页
+        return true;
+      }
+    }
   },
   methods: {
     ...mapMutations(['toggleCompInfoLoading']),
     search () {
       // TODO
       alert(this.keyword);
+    },
+    // TODO: 调用真实接口
+    fetchPage () {
+      alert(this.pageNum);
     }
   }
 };
@@ -57,7 +126,13 @@ export default {
 
 <style lang="scss" scoped>
 .card-company-info {
-  min-height: $comInfoCardHeight;
+  padding: $comInfoListPadding;
+}
+.company-info-item {
+  min-height: $comInfoItemMinHeight;
+}
+.company-info-pagination-container {
+  margin: 0;
 }
 .loading-company-info {
   @include absoluteCenter;
