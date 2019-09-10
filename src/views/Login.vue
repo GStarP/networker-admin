f83973dba980c8e1dceac51eafa9894a48b22250
Weijin1455236662 <1455236662@qq.com>
2019-09-10 16:31:50 +0800

    Revert "feat: 优化登录界面及部分适配"
    This reverts commit 944282c3fe3355cfb445a328afe24fd8e54f1b0d.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,90 +1,78 @@
 <template>
-  <v-container fluid style="height: max-content;padding: 0 !important;">
-    <!--背景图片-->
-    <v-img
-        justify-center
-        :src="this.isPhone ? require('../assets/images/bg-navigator.jpg'): require('../assets/images/bg-login.jpg')"
-        height="100%"
-    >
-      <v-container justify-center :class="{ 'login-box-pc': !isPhone, 'login-box-phone': isPhone }">
-        <v-card>
-          <v-container>
-            <h2>管理员登录</h2>
-            <v-form
-                ref="form"
-                v-model="valid"
+  <!--背景图片-->
+  <v-img
+    src="../assets/images/bg-navigator.jpg"
+    height="100%"
+  >
+    <v-container justify-center class="login-box">
+      <v-card>
+        <v-container col="12" md="4">
+          <h2>管理员登录</h2>
+          <v-form
+            ref="form"
+            v-model="valid"
+          >
+            <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              label="账户名"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="密码"
+              type="password"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="login"
             >
-              <v-text-field
-                  v-model="name"
-                  :rules="nameRules"
-                  label="账户名"
-                  required
-              ></v-text-field>
+              登录
+            </v-btn>
 
-              <v-text-field
-                  v-model="password"
-                  :rules="passwordRules"
-                  label="密码"
-                  type="password"
-                  required
-              ></v-text-field>
-
-              <v-btn
-                  id="confirm"
-                  :disabled="!valid"
-                  color="info"
-                  @click="login"
-              >
-                登录
-              </v-btn>
-
-              <v-btn
-                  id="reset"
-                  color="error"
-                  @click="reset"
-              >
-                清空
-              </v-btn>
-            </v-form>
-          </v-container>
-        </v-card>
-      </v-container>
-    </v-img>
-  </v-container>
+            <v-btn
+              color="error"
+              class="mr-4"
+              @click="reset"
+            >
+              清空
+            </v-btn>
+          </v-form>
+        </v-container>
+      </v-card>
+    </v-container>
+  </v-img>
 </template>
 
 <script>
 export default {
   name: 'Login',
   data: () => ({
-    isPhone: false,
     valid: true,
     name: '',
     nameRules: [
-      v => !!v || '账号不能为空！'
+      v => !!v || 'Name is required'
     ],
     password: '',
     passwordRules: [
-      v => !!v || '密码不能为空！'
+      v => !!v || 'Name is required'
     ]
   }),
-  mounted () {
-    // 先根据屏幕宽度判断设备类型
-    this.onWidthChange();
-    window.addEventListener('resize', this.onWidthChange);
-  },
+
   methods: {
-    onWidthChange () {
-      // 根据宽度判断设备是否为手机
-      this.isPhone = window.innerWidth <= 500;
-    },
     login () {
       if (this.$refs.form.validate()) {
-        if (this.name === 'networker' && this.password === '123') {
+        if (this.name === '围巾女朋友紫宁' && this.password === '123123') {
           // todo:存Cookies还是LocalStorage
-          this.$router.push('/home');
+          this.$router.push('/');
         } else {
-          alert('账号或密码错误啊喂！');
+          alert('密码错误啊喂！');
         }
       }
     },
@@ -96,27 +84,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .login-box-pc{
-    width: 40%;
-    margin-top: 10%;
-    border-radius: 10px;
-  }
-  .login-box-phone{
-    width: 65%;
-    margin-top: 50%;
-    border-radius: 10px;
-  }
-  #confirm{
-    background-color: #2196f3;
-    width: 10% !important;
-    margin-right: 7px;
-  }
-  #reset{
-    background-color: #ff5252;
-    width: 10% !important;
-    margin-left: 7px;
-  }
-  h2{
-    text-align: center;
-  }
+.login-box{
+  width: 50%;
+}
 </style>
